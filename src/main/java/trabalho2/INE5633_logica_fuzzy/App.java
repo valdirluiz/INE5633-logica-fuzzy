fbe140c2e6ba0a157c889879bf5a161d48d10c1d
Valdir Luiz <arnholdvaldir@gmail.com>
2017-11-02 17:57:30 -0200

    Add load de logica e libs necessarias

--- src/main/java/trabalho2/INE5633_logica_fuzzy/App.java
+++ src/main/java/trabalho2/INE5633_logica_fuzzy/App.java
@@ -1,13 +1,18 @@
 package trabalho2.INE5633_logica_fuzzy;
 
+import net.sourceforge.jFuzzyLogic.FIS;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	private static final String FILENAME = "robot-fuzzy.fcl";
+
+	public static void main(String[] args) {
+		System.out.println("Iniciando sistema!");
+		FIS fis = FIS.load(FILENAME, true);
+
+		System.out.println(fis);
+	}
 }
